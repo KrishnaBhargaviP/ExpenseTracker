275f571dcb6231ffe9e6b7a61d448e90cd52d720
KrishnaBhargaviP <w0866008@myscc.ca>
2025-04-01 10:16:42 -0400

    modify expense detail class

--- app/src/main/java/com/example/expensetracker/Expense.kt
+++ app/src/main/java/com/example/expensetracker/Expense.kt
@@ -1,8 +1,12 @@
 package com.example.expensetracker
 
+import android.icu.util.Currency
+
 data class Expense(
     val id: Number,
     val expenseName: String,
     val expenseAmount: Double,
-    val expenseDate: String
+    val expenseDate: String,
+    val currency: Currency,
+    val convertedCost: Double
 )
