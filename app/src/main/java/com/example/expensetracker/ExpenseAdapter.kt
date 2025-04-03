0192478f047f1e23e915f4839f8416a38672aa2f
KrishnaBhargaviP <w0866008@myscc.ca>
2025-04-03 12:00:48 -0400

    remove edit

--- app/src/main/java/com/example/expensetracker/ExpenseAdapter.kt
+++ app/src/main/java/com/example/expensetracker/ExpenseAdapter.kt
@@ -35,7 +35,7 @@ class ExpenseAdapter(
         private val expenseNameTextView: TextView = itemView.findViewById(R.id.expenseNameTextView)
         private val expenseAmountTextView: TextView = itemView.findViewById(R.id.expenseAmountTextView)
         private val expenseDateTextView: TextView = itemView.findViewById(R.id.expenseDateTextView)
-        private val editButton: Button = itemView.findViewById(R.id.editButton)
+//        private val editButton: Button = itemView.findViewById(R.id.editButton)
         private val deleteButton: Button = itemView.findViewById(R.id.deleteButton)
         private val viewButton: Button = itemView.findViewById(R.id.viewButton)
 
@@ -45,9 +45,9 @@ class ExpenseAdapter(
             expenseAmountTextView.text = "Amount: ${sym} ${"%.2f".format(expense.convertedCost)} "
             expenseDateTextView.text = "Date: ${expense.expenseDate}"
 
-            editButton.setOnClickListener {
-                listener.onEditClick(expense)
-            }
+//            editButton.setOnClickListener {
+//                listener.onEditClick(expense)
+//            }
 
             deleteButton.setOnClickListener {
                 listener.onDeleteClick(expense)
